087f2b2ddd0acac41193662428aaa402e955a1be
Majid Ali <itsmajidali129@gmail.com>
2025-07-04 10:18:08 +0500

    fex: type import for sonner

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import { Provider } from "react-redux";
 import { store } from "./store/store.ts";
 import { QueryClient } from "@tanstack/react-query";
 import { QueryClientProvider } from "@tanstack/react-query";
+import { ThemeProvider } from "next-themes";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -19,11 +20,13 @@ const queryClient = new QueryClient({
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </QueryClientProvider>
+      <ThemeProvider>
+        <QueryClientProvider client={queryClient}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </QueryClientProvider>
+      </ThemeProvider>
     </Provider>
   </StrictMode>
 );
